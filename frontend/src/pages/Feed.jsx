46617048a702eac74f1fa42e5cf3e64df3eec311
Manash2005 <swainm099@gmail.com>
2026-02-09 01:52:48 +0530

    fetched all the posts to the frontend

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState,  useEffect} from 'react'
+import axios from "axios"
 
 
 function Feed() {
@@ -10,6 +11,12 @@ function Feed() {
             caption : "ImageKIt"
         }
     ])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/posts/").then((res) => {
+            setPosts(res.data.posts)
+        })
+    } , [])
 
   return (
     <section className="feed-section">
